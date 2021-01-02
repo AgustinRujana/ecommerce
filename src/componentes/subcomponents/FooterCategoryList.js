0cd119f787731b223866400a6ff6c562e470021b
Agustin Rujana <aguru2008@hotmail.com>
2021-01-02 18:58:47 -0300

    commit

--- src/componentes/subcomponents/FooterCategoryList.js
+++ src/componentes/subcomponents/FooterCategoryList.js
@@ -1,12 +1,11 @@
 import Media from 'react-bootstrap/Media';
 import { Link } from 'react-router-dom';
 
+//Icons
 import CarrotIcon from '../icons/img/CarrotIcon_White.svg';
 import RadishIcon from '../icons/img/RadishIcon_White.svg';
 import OilIcon from '../icons/img/OilIcon_White.svg';
-import FlourIcon from '../icons/img/FlourIcon_White.svg';
 import SeedsIcon from '../icons/img/SeedsIcon_White.svg';
-
 
 const FooterCategoryList = () => {
 
@@ -14,25 +13,25 @@ const FooterCategoryList = () => {
     <Media>
         <img width={32} height={32} className="mr-3" src={CarrotIcon} alt="Generic placeholder"/>
         <Media.Body>
-            <Link to={`/categories/Frutas`}><p className='mt-1 FooterLink'>Frutas</p></Link>
+            <Link to={`/categories/frutas`}><p className='mt-1 FooterLink'>Frutas</p></Link>
         </Media.Body>
     </Media>
     <Media>
         <img width={32} height={32} className="mr-3" src={RadishIcon} alt="Generic placeholder"/>
         <Media.Body>
-            <Link to={`/categories/Verduras`}><p className='mt-1 FooterLink'>Verduras</p></Link>
+            <Link to={`/categories/verduras`}><p className='mt-1 FooterLink'>Verduras</p></Link>
         </Media.Body>
     </Media>
     <Media>
         <img width={32} height={32} className="mr-3" src={OilIcon} alt="Generic placeholder"/>
         <Media.Body>
-            <Link to={`/categories/Aceites`}><p className='mt-1 FooterLink'>Aceites</p></Link>
+            <Link to={`/categories/aceites`}><p className='mt-1 FooterLink'>Aceites</p></Link>
         </Media.Body>
     </Media>
     <Media>
         <img width={32} height={32} className="mr-3" src={SeedsIcon} alt="Generic placeholder"/>
         <Media.Body>
-            <Link to={`/categories/Semillas`}><p className='mt-1 FooterLink'>Semillas</p></Link>
+            <Link to={`/categories/semillas`}><p className='mt-1 FooterLink'>Semillas</p></Link>
         </Media.Body>
     </Media>
     </>
